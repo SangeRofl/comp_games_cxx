b038ddaa9f5f83596b89697a099c8f8b5612c683
Danila2433 <Dudkov_2002@mail.com>
2022-12-28 20:32:31 +0300

    new commit

--- addGameInfoModule/agents/py_utils/gamestore.py
+++ addGameInfoModule/agents/py_utils/gamestore.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import argparse
+import os
 parser = argparse.ArgumentParser(description = 'getting an age rating')
 parser.add_argument("-agent",dest = 'name')
 parser.add_argument("-site",dest = 'url')
@@ -9,10 +10,9 @@ args = parser.parse_args()
 if args.name == "ageRating":
     r = requests.get(args.url)
     soup = BeautifulSoup(r.text, 'html.parser')
-    items = soup.find_all('div', class_='game_rating_agency')
-    f = open("../problem-solver/cxx/addGameInfoModule/agents/py_utils/data/age_rating1.txt", "w")
+    items = soup.find_all('div', class_="item-info")[3]
+    items = items.find_all("/strong")
+    f = open("../problem-solver/cxx/addGameInfoModule/agents/py_utils/data/age_rating.txt", "w")
     for item in items:
-      f.write(item.text.strip())
+        f.write(item.text.strip())
     f.close()
-
-
